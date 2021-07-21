402fbb37ec59202e6b596698d779f69bec9bf893
karolow <karol.piekarski@gmail.com>
2021-07-21 22:15:16 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     description='A Python package to batch geocode address points in the city of Katowice, Poland.',
     packages=find_packages(include=['geocoder', 'geocoder.*']),
+    python_requires='>=3.6, <=3.9',
     entry_points={
         'console_scripts': ['geocoder=geocoder.cli:cli'],
     },
